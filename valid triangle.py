c5dc87561b2aed40d4ae0aec86b4c091eb041df9
PatelHeer06 <heerpatel2607@gmail.com>
2025-04-25 19:28:56 +0530

    Delete valid triangle.py

--- valid triangle.py
+++
@@ -1,8 +0,0 @@
-#valid triangle
-a=int(input("Enter first angle:"))
-b=int(input("Enter second angle:"))
-c=int(input("Enter third angle:"))
-if(a+b+c==180):
-    print("Valid Triangle")
-else:
-    print("Not Valid Triangle")
