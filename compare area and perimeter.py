54964d48bbffd04de04147d556130e0f1df747e0
PatelHeer06 <heerpatel2607@gmail.com>
2025-04-25 19:20:12 +0530

    Delete compare area and perimeter.py

--- compare area and perimeter.py
+++
@@ -1,9 +0,0 @@
-#area greater than perimeter
-a=int(input("Enter length:"))
-b=int(input("Enter breadth:"))
-c=a*b
-d=2*(a+b)
-if(c>d):
-    print("Area is greater")
-else:
-    print("Perimeter is greater")
