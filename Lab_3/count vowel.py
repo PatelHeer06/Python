1d70a4ad5fed48e479ab085aad5fb17df4f8e9f6
PatelHeer06 <heerpatel2607@gmail.com>
2025-04-26 10:08:01 +0530

    Add files via upload

---
+++ Lab_3/count vowel.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 10:42:39 2025
+
+@author: oener
+"""
+
+def vow():
+    a=input("Enter a string")
+    b=a.count("a")+a.count("e")+a.count("i")+a.count("o")+a.count("u")+a.count("A")+a.count("E")+a.count("I")+a.count("O")+a.count("U")
+    print(b)
+vow()
