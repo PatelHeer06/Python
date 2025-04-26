3a97b34e430062887fccf2fcc2cdd38a3f5925b1
PatelHeer06 <heerpatel2607@gmail.com>
2025-04-26 10:15:38 +0530

    Add files via upload

---
+++ Lab_7/check empty dictionary.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 08:17:42 2025
+
+@author: oener
+"""
+
+d1={}
+if (len(d1)==0):
+    print("Empty dictionary")
+else:
+    print("Not Empty dictionary")
---
+++ Lab_7/concate one dict into another.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 27 08:13:02 2025
+
+@author: oener
+"""
+
+d1={1:'Heer',2:'Patel'}
+d2={3:'Bhavya',4:'Jain'}
+d3={5:'Yashvi',6:'Majmudar'}
+d4={**d1,**d2,**d3}
+print(d4)
