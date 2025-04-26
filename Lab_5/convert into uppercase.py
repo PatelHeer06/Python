4ec3a59019d28dc158842227f0cee0bb0ee23334
PatelHeer06 <heerpatel2607@gmail.com>
2025-04-26 10:12:32 +0530

    Add files via upload

---
+++ Lab_5/convert into uppercase.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 28 09:36:32 2025
+
+@author: oener
+"""
+
+l=['heer','tirth','krissa','vruti','devarya']
+l1=[]
+for i in l:
+    a=i.upper()
+    l1.append(a)
+print(l1)
